cc703741aae5529e9653d1b39e953b24e93c329f
samiksha kolhe <73512374+kolhesamiksha@users.noreply.github.com>
2024-08-15 18:19:54 +0530

    chore:get github_token from mongo

--- Chatbot-streamlit/src/utils/get_insert_mongo_data.py
+++ Chatbot-streamlit/src/utils/get_insert_mongo_data.py
@@ -13,14 +13,14 @@ def format_creds_mongo():
     mongo_dict = {}
     item_details = get_creds_from_mongo(CONNECTION_STRING)
     for items in item_details:
-        if items['cred_name']=="OPENAI_API_KEY":
-            mongo_dict['OPENAI_API_KEY'] = items['cred_values']
-        if items['cred_name']=="OPENAI_API_BASE":
-            mongo_dict['OPENAI_API_BASE'] = items['cred_values']
         if items['cred_name']=="GROQ_API_KEY":
             mongo_dict['GROQ_API_KEY'] = items['cred_values']
         if items['cred_name']=="ZILLIZ_CLOUD_URI":
             mongo_dict['ZILLIZ_CLOUD_URI'] = items['cred_values']
         if items['cred_name']=="ZILLIZ_CLOUD_API_KEY":
             mongo_dict['ZILLIZ_CLOUD_API_KEY'] = items['cred_values']
+        if items['cred_name']=="ZILLIZ_CLOUD_API_KEY":
+            mongo_dict['ZILLIZ_CLOUD_API_KEY'] = items['cred_values']
+        if items['cred_name']=="GITHUB_TOKEN":
+            mongo_dict['GITHUB_TOKEN'] = items['cred_values']
     return mongo_dict
