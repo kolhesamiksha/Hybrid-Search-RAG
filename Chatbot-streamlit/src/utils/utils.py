37c1e26280324241bda653437232110ad4ee6b46
kolhesamiksha <kolhesamiksha25@gmail.com>
2024-08-17 18:03:35 +0530

    chore:update decrypt_pass to accept base64 encrypted data

--- Chatbot-streamlit/src/utils/utils.py
+++ Chatbot-streamlit/src/utils/utils.py
@@ -3,9 +3,11 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 import ast
+import base64
 
-def decrypt_pass(encrypted_key):
-    encrypted_key = ast.literal_eval(encrypted_key)
+def decrypt_pass(base64_encoded_key):
+    encrypted_key = base64.b64decode(base64_encoded_key)
+    # encrypted_key = ast.literal_eval(encrypted_key)
     iv = encrypted_key[:AES.block_size]  # Extract the IV
     cipher_text = encrypted_key[AES.block_size:]  # Extract the ciphertext
     cipher = AES.new(iv, AES.MODE_CBC, iv)  # Recreate the cipher with the extracted IV
