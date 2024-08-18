f56770b47112cd6f97f1a0a97dea5c0c240f9655
kolhesamiksha <kolhesamiksha25@gmail.com>
2024-08-19 00:37:06 +0530

    chore:update

--- Chatbot-streamlit/src/api.py
+++ Chatbot-streamlit/src/api.py
@@ -367,10 +367,10 @@ def advance_rag_chatbot(question, history):
                 output = milvus_hybrid_search(question, expr="")
                 combined_results.extend(output)
             # reranked_docs = Reranker(question, combined_results)
-            formatted_context = format_docs(reranked_docs)
+            formatted_context = format_docs(combined_results)
             response = chatbot(question, formatted_context, history)
             end_time = time.time() - st_time
-            return (response, end_time, reranked_docs)
+            return (response, end_time, combined_results)
     except Exception as e:
         print(f"ERROR: {traceback.format_exc()}")
         end_time = time.time() - st_time
