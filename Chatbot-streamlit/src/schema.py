63406b625683110e4deb69e714ce6160e1559467
kolhesamiksha <kolhesamiksha25@gmail.com>
2024-08-18 14:57:21 +0530

    chore:update schema name

--- Chatbot-streamlit/src/schema.py
+++ Chatbot-streamlit/src/schema.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from typing import List, Tuple
 
-class PredictSchema(BaseModel):
+class ResponseSchema(BaseModel):
     query: str
     history: List[Tuple[str, str]] = []
