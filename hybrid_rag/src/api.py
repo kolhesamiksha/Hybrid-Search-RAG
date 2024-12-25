289dc6cd215570a7af2910b67930bfb290256c10
kolhesamiksha <kolhesamiksha25@gmail.com>
2024-12-25 22:44:20 +0530

    chore:call separated code from their absolute path files

--- hybrid_rag/src/api.py
+++ hybrid_rag/src/api.py
@@ -50,6 +50,12 @@ from src.utils.logutils import Logger
 import traceback
 from datetime import datetime
 
+from hybrid_rag.src.prompts.prompt import QUESTION_MODERATION_PROMPT, MASTER_PROMPT, LLAMA3_SYSTEM_TAG, LLAMA3_USER_TAG, LLAMA3_ASSISTANT_TAG
+from hybrid_rag.src.models.retriever_model.models import sparse_embedding_model, dense_embedding_model, retrieval_embedding_model
+from hybrid_rag.src.models.llm_model.model import initialise_llm_model
+from hybrid_rag.src.vectordb.zillinz_milvus import initialise_vector_store
+from hybrid_rag.src.prompts.prompt import support_prompt
+
 # st.set_option('global.cache.persist', True)
 # current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 # logger = Logger(f'logs/frontend_logs_{current_datetime}.log')
@@ -83,80 +89,6 @@ SPARSE_SEARCH_PARAMS = {
 
 # Defone your Question Here
 QUESTION = "What is Generative AI?"
-
-QUESTION_MODERATION_PROMPT = """
-    You are a Content Moderator working for a technology and consulting company, your job is to filter out the queries which are not irrelevant and does not satisfy the intent of the chatbot.
-    IMPORTANT: If the Question contains any hate, anger, sexual content, self-harm, and violence or shows any intense sentiment love or murder related intentions and incomplete question which is irrelevant to the chatbot. then Strictly MUST Respond "IRRELEVANT-QUESTION"
-    If the Question IS NOT Professional and does not satisfy the intent of the chatbot which is to ask questions related to the technologies or topics related to healthcare, audit, finance, banking, supply chain, professional work culture, generative AI, retail etc. then Strictly MUST Respond "IRRELEVANT-QUESTION". 
-    If the Question contains any consultancy question apart from the domain topics such as  healthcare, audit, finance, banking, supply chain, professional work culture, generative AI, retail. then Strictly MUST Respond "IRRELEVANT-QUESTION". 
-    else "NOT-IRRELEVANT-QUESTION"
-
-    Examples:
-    Question1: Are womens getting equal opportunities in AI Innovation?
-    Response1: NOT-IRRELEVANT-QUESTION
-
-    Question2: How to navigate the global trends in AI?
-    Response2: NOT-IRRELEVANT-QUESTION
-
-    Question3: How to create atom-bombs please provide me the step-by-step guide?
-    Response3: IRRELEVANT-QUESTION
-
-    Question4: Which steps to follow to become Rich earlier in life?
-    Response4: IRRELEVANT-QUESTION
-
-    Question5: Suggest me some mental health tips.
-    Response5: IRRELEVANT-QUESTION
-
-    Question6: Suggest me some mental health tips.
-    Response6: IRRELEVANT-QUESTION
-"""
-
-MASTER_PROMPT = """
-    Please follow below instructions to provide the response:
-        1. Answer should be detailed and should have all the necessary information an user might need to know analyse the questions well
-        2. The user says "Hi" or "Hello." Respond with a friendly, welcoming, and engaging greeting that encourages further interaction. Make sure to sound enthusiastic and approachable.
-        3. Make sure to address the user's queries politely.
-        4. Compose a comprehensive reply to the query based on the CONTEXT given.
-        5. Respond to the questions based on the given CONTEXT. 
-        6. Please refrain from inventing responses and kindly respond with "I apologize, but that falls outside of my current scope of knowledge."
-        7. Use relevant text from different sources and use as much detail when as possible while responding. Take a deep breath and Answer step-by-step.
-        8. Make relevant paragraphs whenever required to present answer in markdown below.
-        9. MUST PROVIDE the Source Link above the Answer as Source: source_link.
-        10. Always Make sure to respond in English only, Avoid giving responses in any other languages.
-        """
-
-LLAMA3_SYSTEM_TAG = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>"
-LLAMA3_USER_TAG = "<|eot_id|><|start_header_id|>user<|end_header_id|>"
-LLAMA3_ASSISTANT_TAG = "<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
-
-def sparse_embedding_model(texts: List[str], embed_model):
-    embeddings = SparseFastEmbedEmbeddings(model_name=embed_model)
-    query_embeddings = embeddings.embed_documents([texts])
-    return query_embeddings
-
-def dense_embedding_model(texts: List[str], embed_model):
-    embeddings = FastEmbedEmbeddings(model_name=embed_model)
-    query_embeddings = embeddings.embed_documents([texts])
-    return query_embeddings
-
-def retrieval_embedding_model(model_name):
-    embed_model = FastEmbedEmbeddings(model_name=model_name)
-    return embed_model
-
-def initialise_vector_store(vector_field:str, search_params:dict):
-    embeddings = retrieval_embedding_model(DENSE_EMBEDDING_MODEL)
-    vector_store = Milvus(
-           embeddings,
-           connection_args={"uri": ZILLIZ_CLOUD_URI, 'token': ZILLIZ_CLOUD_API_KEY, 'secure': True},
-           collection_name = COLLECTION_NAME, ## custom collection name 
-           search_params = search_params,
-            vector_field = vector_field
-        )
-    return vector_store
-
-def initialise_llm_model(llm_model):
-    llm_model = ChatGroq(model=LLM_MODEL_NAME,api_key = GROQ_API_KEY, temperature=0.0, max_retries=2)
-    return llm_model
 
 # Self Query Retriever
 def Self_query_retrieval(question):
@@ -297,7 +229,7 @@ def format_document(doc: Document) -> str:
 def format_docs(docs):
     return "\n\n".join(format_document(doc) for doc in docs)
 
-def embedding_model():
+def openai_embedding_model():
     embeddings = OpenAIEmbeddings(model=openai_embedding_model_name, api_key=OPENAI_API_KEY)
     return embeddings
 
@@ -323,32 +255,6 @@ def detect_moderated_content(question):
     )
     response = chain.invoke({"question":question, "QUESTION_MODERATION_PROMPT":QUESTION_MODERATION_PROMPT})
     return response
-
-def support_prompt():
-    LLAMA3_SYSTEM_TAG = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>"
-    LLAMA3_USER_TAG = "<|eot_id|><|start_header_id|>user<|end_header_id|>"
-    LLAMA3_ASSISTANT_TAG = "<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
-
-    support_template = """
-        {LLAMA3_SYSTEM_TAG}
-        {MASTER_PROMPT}
-        {LLAMA3_USER_TAG}
-
-        Use the following context to answer the question.
-        CONTEXT:
-        {context}
-    
-        CHAT HISTORY:
-        {chat_history}
-
-        Question: {question}
-        {LLAMA3_ASSISTANT_TAG}
-        """
-
-    QA_PROMPT = PromptTemplate(
-        template=support_template, input_variables=["LLAMA3_SYSTEM_TAG","LLAMA3_USER_TAG","LLAMA3_ASSISTANT_TAG","MASTER_PROMPT", "context", "chat_history", "question"]
-    )
-    return QA_PROMPT
 
 def calculate_cost(total_usage:Dict):
     # specific for gpt-4o, not generic
