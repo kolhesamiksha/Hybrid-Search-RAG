bf455d94655d74251c939c776586984da297dc75
kolhesamiksha <kolhesamiksha25@gmail.com>
2024-12-27 00:13:02 +0530

    chore:add count_tokens, save_data_to_github inside utils

--- hybrid_rag/src/utils/utils.py
+++ hybrid_rag/src/utils/utils.py
@@ -1,47 +1,20 @@
-import hashlib
 import io
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
-from Crypto.Random import get_random_bytes
 import ast
 import base64
+from typing import List, Dict
 from github import Github
 import pandas as pd
 from typing import List
 import traceback
 
-from ragas.metrics import faithfulness,context_utilization
-from ragas.metrics.critique import harmfulness, correctness
-from langchain_openai import ChatOpenAI
-from ragas import evaluate
-from langchain_core.documents import Document
-from datasets import Dataset, Sequence
-from ragas.llms import LangchainLLMWrapper
-from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from hybrid_rag.src.utils.logutils import Logger
 
-from .get_insert_mongo_data import format_creds_mongo
-creds_mongo = format_creds_mongo()
+#TODO:add Logger & exceptions
 
-DENSE_EMBEDDING_MODEL = "jinaai/jina-embeddings-v2-base-en"
-LLM_MODEL_NAME = "llama-3.1-70b-versatile"
-OPENAI_API_BASE = creds_mongo['OPENAI_API_BASE']
 
-def decrypt_pass(base64_encoded_key):
-    encrypted_key = base64.b64decode(base64_encoded_key)
-    # encrypted_key = ast.literal_eval(encrypted_key)
-    iv = encrypted_key[:AES.block_size]  # Extract the IV
-    cipher_text = encrypted_key[AES.block_size:]  # Extract the ciphertext
-    cipher = AES.new(iv, AES.MODE_CBC, iv)  # Recreate the cipher with the extracted IV
-    padded_plain_text = cipher.decrypt(cipher_text)
-    plain_text = unpad(padded_plain_text, AES.block_size)
-    return plain_text.decode('utf-8')
-
-GTHUB_TOKEN = decrypt_pass(creds_mongo['GITHUB_TOKEN'])
-GROQ_API_KEY = decrypt_pass(creds_mongo['GROQ_API_KEY'])
-
-def save_history_to_github(query, response):
+def save_history_to_github(query, response, github_token):
     try:
-        g = Github(GTHUB_TOKEN)
+        g = Github(github_token)
         repo = g.get_repo("kolhesamiksha/Hybrid-Search-RAG")
         contents = repo.get_contents('Chatbot-streamlit/chat_history/chat_history.csv')
         decoded_content = base64.b64decode(contents.content)
@@ -53,59 +26,16 @@ def save_history_to_github(query, response):
         repo.update_file(contents.path, "Updated CSV File", updated_csv, contents.sha)
     except Exception as e:
         print(traceback.format_exc())
+    
 
-def validate_column_dtypes(ds: Dataset):
-    for column_names in ["question", "answer", "ground_truth"]:
-        try:
-            if column_names in ds.features:
-                if ds.features[column_names].feature.dtype != "string":
-                    raise ValueError(
-                        f'Dataset feature "{column_names}" should be of type string'
-                    )
-                    return "FAIL"
-                else:
-                    return "PASS"
-        except Exception as e:
-            print(traceback.format_exc())
-            return "FAIL"
+def calculate_cost(total_usage:Dict):
+    # specific for gpt-4o, not generic
+    completion_tokens = total_usage['token_usage']['completion_tokens']
+    prompt_tokens = total_usage['token_usage']['prompt_tokens']
 
-    for column_names in ["contexts"]:
-        try:
-            if column_names in ds.features:
-                if not (
-                    isinstance(ds.features[column_names], Sequence)
-                    and ds.features[column_names].feature.dtype == "string"
-                ):
-                    raise ValueError(
-                        f'Dataset feature "{column_names}" should be of type'
-                        f" Sequence[string], got {type(ds.features[column_names])}"
-                    )
-                    return "FAIL"
-                else:
-                    return "PASS"
-        except Exception as e:
-            print(traceback.format_exc())
-            return "FAIL"
+    #cost in $
+    input_token = (prompt_tokens/1000)*0.0065
+    output_token = (completion_tokens/1000)*0.0195
 
-def rag_evaluation(question:List[str], answer:List[str], context:List[List[str]]):
-    try:
-        data = {
-            "question": question,
-            "answer": answer,
-            "contexts": context
-        }
-        rag_dataset = Dataset.from_dict(data)
-        llm_chat = ChatOpenAI(
-            model=LLM_MODEL_NAME,
-            openai_api_base=OPENAI_API_BASE,
-            openai_api_key=GROQ_API_KEY
-        )
-        
-        evaluation_chat_model = LangchainLLMWrapper(llm_chat)
-        evaluation_embeddings = FastEmbedEmbeddings(model_name=DENSE_EMBEDDING_MODEL)
-        result = evaluate(rag_dataset,metrics=[faithfulness,context_utilization, harmfulness, correctness],
-                            llm=evaluation_chat_model, embeddings=evaluation_embeddings)
-        return result
-    except Exception as e:
-        print(traceback.format_exc())
-        return {}
+    total_cost = input_token+output_token
+    return total_cost
