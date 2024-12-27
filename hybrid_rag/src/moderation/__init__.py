cdc6a21ac536e542c7551d540a5f82e42ce099cb
kolhesamiksha <kolhesamiksha25@gmail.com>
2024-12-28 02:22:35 +0530

    feat:add question moderation * navigation as separate module

---
+++ hybrid_rag/src/moderation/__init__.py
@@ -0,0 +1,1 @@
+from .question_moderator import QuestionModerator
--- hybrid_rag/src/moderation/question_moderator.py
+++ hybrid_rag/src/moderation/question_moderator.py
@@ -1,4 +1,7 @@
 import os
+from typing import Optional
+
+from hybrid_rag.src.utils.logutils import Logger
 from langchain_core.prompts.prompt import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from hybrid_rag.src.models.llm_model.model import LLMModelInitializer
@@ -8,16 +11,21 @@ class QuestionModerator:
     A class to detect moderated content using a language model and a moderation prompt.
     """
 
-    def __init__(self, llm_model_name: str, groq_api_key:str):
+    def __init__(self,
+                 llmModelInstance: LLMModelInitializer,
+                 logger: Optional[Logger]=None
+                ):
         """
         Initialize the ModerationContentDetector with an LLM model.
 
-        :param llm_model_name: The name of the LLM model to be used.
+        :param llmModelInstance: LLMModel Instance must be of type LLMModelInitializer class
         """
-        self.llm_model_name = llm_model_name
-        self.__groq_api_key = groq_api_key
-        
-        llmModelInitializer = LLMModelInitializer(self.llm_model_name, self.__groq_api_key)
+        #self.llm_model_name = llm_model_name
+        #self.__groq_api_key = groq_api_key
+        self.llmModelInstance = llmModelInstance
+        #llmModelInitializer = LLMModelInitializer(self.llm_model_name, self.__groq_api_key)
+
+        llmModelInitializer = self.llmModelInstance
         self.llm_model_instance = llmModelInitializer.initialise_llm_model()
         
         self.prompt_template = """
