5dd75f18cfe4f940fed71756a5d414a23220c144
kolhesamiksha <kolhesamiksha25@gmail.com>
2025-01-04 19:42:17 +0530

    chore:update code

--- chat_restapi/api.py
+++ chat_restapi/api.py
@@ -1,4 +1,8 @@
-# to install the env variables from .env file
+# Create a .env.example file with all Env variables added
+
+import os
+os.system("pip install hybrid_rag-0.0.1-py3-none-any.whl")
+
 from typing import List
 from typing import Tuple
 
@@ -10,7 +14,6 @@ from hybrid_rag.src.rag import RAGChatbot
 from hybrid_rag.src.utils import Logger
 from pydantic import BaseModel
 
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 rag_router = APIRouter()
 
 
@@ -18,9 +21,10 @@ class ResponseSchema(BaseModel):
     query: str
     history: List[Tuple[str, str]] = []
 
-@rag_router.post("/predict")
+@rag_router.post("/chatbot")
 async def pred(response: Response, elements: ResponseSchema):
     load_dotenv(dotenv_path=".env.example")
+
     question = "tell me about supply chain consulting"
     history = []
     logger = Logger().get_logger()
