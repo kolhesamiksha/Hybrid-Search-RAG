993c23bff06484d2f31eefad2be69ff32a6ca1f7
kolhesamiksha <kolhesamiksha25@gmail.com>
2025-01-22 19:02:06 +0530

    initial commit

---
+++ hybrid_rag/src/utils/followup_questions.py
@@ -0,0 +1,11 @@
+"""
+Module Name: hybrid_search.py
+Author: Samiksha Kolhe
+Version: 0.1.0
+"""
+
+#question & response 
+
+#summarization support
+
+#guardrails support: ingrained inside the module 
